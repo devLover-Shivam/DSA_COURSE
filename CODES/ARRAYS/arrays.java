37c8904d1bbeaf1a792ce5720cd9266bbe4e71b0
Shivam Kumar Gupta <shivamkumargupta109@gmail.com>
2026-02-15 20:02:04 +0530

    Add Reverse Sub Array

--- CODES/ARRAYS/arrays.java
+++ CODES/ARRAYS/arrays.java
@@ -36,6 +36,33 @@ public class arrays {
             System.out.print(arr[i]+" ");
         }
     }
+    
+    //Reversing Sub Array
+
+    public static void reverseSubArray(int arr[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the starting index to be reversed : ");
+        int s = sc.nextInt();
+
+        System.out.println("Enter the ending index to be reversed : ");
+        int e = sc.nextInt();
+
+        int sp = s;
+        int ep =e;
+
+        while(sp<ep){
+            int temp =arr[sp];
+            arr[sp] = arr[ep];
+            arr[ep] = temp;
+            sp++;
+            ep--;
+        }
+
+        System.out.print("Reversed Sub array is: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -45,7 +72,7 @@ public class arrays {
             arr[i] = sc.nextInt();
         }
 
-        reverseArray(arr);
+        reverseSubArray(arr);
 
         
     }
